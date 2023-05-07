8533ca7ad233697e6f58cd67da53d6b0ab8c5015
ferschoneich01 <59126953+ferschoneich01@users.noreply.github.com>
2023-05-07 06:30:57 +0000

    Sun, May 7, 2023, 12:30 AM -06:00

--- src/application.py
+++ src/application.py
@@ -35,12 +35,34 @@ def index():
 @app.route("/signIn", methods=["POST"])
 def signIn():
     if request.method == 'POST':
-        #obtenemos 
+        #obtenemos valores del formulario
         username = request.form.get("username")
         password = request.form.get("password")
 
+        if not request.form.get("username"):
+            flash('Ingrese un nombre de usuario')
+            return redirect("/signIn")
 
-    return render_template("login.html", ruta="Login")
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash('Ingrese una contraseña')
+            return redirect("/signIn")
+
+        user = db.execute(
+            text("SELECT * FROM users WHERE username = '"+str(username)+"'")).fetchall()
+
+        # Ensure username exists and password is correct
+        if len(user) != 1 or not check_password_hash(user[0][2], password):
+            flash('Contraseña Incorrecta')
+            return redirect("/login")
+
+            # Remember which user has logged in
+            session["id_user"] = user[0][0]
+            session["username"] = username
+            session["role_user"] = user[0][5]
+            return redirect('/')
+        else:
+            return render_template("login.html", ruta="Login")
 
 
 @app.route("/sub")
